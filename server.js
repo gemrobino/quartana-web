7062ca82ba39c98c238cb3cc0312f8331b997232
gemrobino <gemrobino@gmail.com>
2023-10-16 16:56:48 -0400

    move index.html to root

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/src'));
 app.use('/images', express.static(__dirname + 'src/images'));
@@ -9,11 +10,7 @@ app.use('/styles', express.static(__dirname + '/styles'));
 app.use('/js', express.static(__dirname + '/js'));
 
 app.get("/", (req,res) => {
-  res.sendFile(__dirname + "/src/index.html")
-})
-
-app.get("/home", (req,res) => {
-  res.sendFile(__dirname + "/src/index.html")
+  res.sendFile(__dirname + "/index.html")
 })
 
 app.get("/dashboard", (req,res) => {
